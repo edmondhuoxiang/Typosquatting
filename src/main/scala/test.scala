d52f8290e510c4553d8aca8effc1d5c8c0098768
Xiang Huo <huoxiang5659@gmail.com>
2013-10-22 00:23:16 -0500

    Can get some pairs and their distribution

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -217,6 +217,132 @@ object test extends Serializable {
 		}
 	}
 
+	def compareIpAddr(ip_addr1:String, ip_addr2:String): Boolean = {
+		//println(ip_addr1)
+		//println(ip_addr2)
+		val ipArr1 = ip_addr1.split('.')
+		val ipArr2 = ip_addr2.split('.')
+		if(ipArr1.length < 4 || ipArr2.length < 4)
+			return false
+		if(ipArr1.apply(0) == ipArr2.apply(0) && ipArr1.apply(1) == ipArr2.apply(1))
+			return true
+		else
+			return false
+	}
+
+	def getSingleCount(popDomain: String, typoDomain: String, records: spark.RDD[(Int, Int, String, String, String, Int, Int, List[Array[String]], List[Array[String]], List[Array[String]])]): Long = {
+		val set = records.filter(x => {
+			val tmp = new parseUtils().parseDomain(x._5, popDomain)
+			tmp == typoDomain
+		})
+		return set.count
+	}
+
+	def getJoinCount(popDomain: String, typoDomain: String, records: spark.RDD[(Int, Int, String, String, String, Int, Int, List[Array[String]], List[Array[String]], List[Array[String]])]): Long = {
+		val typoArr = records.filter(x => {
+			val tmp = new parseUtils().parseDomain(x._5, popDomain)
+			tmp == typoDomain
+		}).toArray
+
+		val popRDD = records.filter(x =>{
+			val tmp = new parseUtils().parseDomain(x._5, popDomain)
+			tmp == popDomain
+		})
+		var count = 0.toLong
+		if(typoArr.length == 0)
+			return 0.toLong
+		for(typo <- typoArr){
+			val tmpCount = popRDD.filter(rec => {
+				val duration = rec._1 - typo._1
+				val flag = compareIpAddr(rec._3, typo._3)
+				duration > 0 && duration < 60 && flag
+			}).count
+			count += tmpCount
+		}
+
+		return count
+	}
+
+	def isPopular(domain: String, popWebArr: Array[String]): Boolean = {
+		val domainBuffer = new scala.collection.mutable.StringBuilder()
+		domainBuffer.append(domain)
+		if(!domain.endsWith("."))
+			domainBuffer.append(".")
+		val popWebBuffer = new scala.collection.mutable.StringBuilder()
+		for(popWeb <- popWebArr){
+			popWebBuffer.clear
+			popWebBuffer.append(popWeb)
+			if(popWeb.endsWith("."))
+				popWebBuffer.append(".")
+			if(popWebBuffer.toString == domainBuffer.toString)
+				return true
+		}
+		return false
+	}
+
+	def getDistribution(sc: SparkContext): Unit = {
+		val inFileDir = "./res/domainAndTypo/"
+		val webFilesDir = "./webfiles/"
+		val outPutDirPath = "./res/distribution/"
+		val popWebFile = "./weblist/top1000"
+
+		val outPutDir = new File(outPutDirPath)
+		if(!outPutDir.exists()){
+			outPutDir.mkdir
+		}
+
+		val popWebArr = sc.textFile(popWebFile).toArray
+
+		val domainAndTypoFileList =  new ListFiles().recursiveListFiles(new File(inFileDir))
+		val webFilesList = new ListFiles().recursiveListFiles(new File(webFilesDir))
+
+		for(domainFile <- domainAndTypoFileList){
+			val domainAndTypo = sc.textFile(domainFile.toString).toArray
+			val outFileWriter = new FileWriter(outPutDirPath + domainFile.getName, false)
+			val outFileBufferWriter = new BufferedWriter(outFileWriter)
+			for(line <- domainAndTypo){
+				val domainsArr = line.split(" ")
+				if(domainsArr.length>1){
+					val popDomain = domainsArr.apply(0)
+					//Check whether the file named as popDomain exists
+					var flag = false
+					var tmpName = ""
+					for(fileTmp <- webFilesList){
+						breakable{
+							val name = fileTmp.getName
+							if((name+".") == popDomain){
+								flag = true
+								tmpName = name
+								break
+							}
+						}
+					}
+					if(flag){
+
+						val dnsRecords = sc.textFile(webFilesDir+tmpName).map(x => {
+							new ParseDNSFast().convert(x)
+						})
+						for(typoCandidate <- domainsArr){
+							if(typoCandidate != popDomain){
+								//a typo cannot be a popular websites
+								val whetherPop = isPopular(typoCandidate, popWebArr)
+								if(!whetherPop){
+									val countSingle = getSingleCount(popDomain, typoCandidate, dnsRecords)
+									val countJoin = getJoinCount(popDomain, typoCandidate, dnsRecords)
+
+									if(countSingle != 0 && (countJoin.toFloat/countSingle.toFloat) > 0.1){
+										outFileBufferWriter.write(typoCandidate+","+popDomain+","+(countJoin.toFloat/countSingle.toFloat)+","+countJoin+","+countSingle+"\n")
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			outFileBufferWriter.close
+
+		}
+	}
 
 	def main(args: Array[String]): Unit = {
 		System.setProperty("spark.default.parallelism","500")
@@ -231,8 +357,8 @@ object test extends Serializable {
 
 	  	//dealWithZoneFiles(sc)
 	  	//generateDomainTypoList(sc)
-	  	cleanAndDivide(sc)
-
+	  	//cleanAndDivide(sc)
+	  	getDistribution(sc)
 
 
 	  /*	val dir = new File(inputPath)
