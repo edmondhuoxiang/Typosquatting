bc30e5f789ed8b2676b3ca08c67c525f2a26eae8
Xiang Huo <huoxiang5659@gmail.com>
2013-11-09 23:52:08 -0600

    Added limitation of TLL

--- src/main/scala/sorted.scala
+++ src/main/scala/sorted.scala
@@ -39,7 +39,6 @@ object sorted extends Serializable {
 	def convertFilename(str: String): String = {
 		val tmp = str.split('/')
 		val filename = tmp.apply(tmp.length-2)
-
 		val filenameArr = filename.split('.')
 		val outFilename = new scala.collection.mutable.StringBuilder()
 		var index = 0
@@ -101,7 +100,7 @@ object sorted extends Serializable {
 		val outFileDir = "./res/domainAndTypo/"
 		val webFiles = "/Users/edmond/Typosquatting/weblist/500_1000"
 
-		val popWebsites = sc.textFile(webFiles).toArray
+		val popWebsites = sc.textFile(webFiles, 20).toArray
 
 		//get list of all files
 		val fileList = new ListFiles().recursiveListFiles(new File(inFileDir))
@@ -190,9 +189,81 @@ object sorted extends Serializable {
 			return false
 	}
 
-	def getPairsFromFile(sc: SparkContext, typoArr: Array[String], inFile: File, outFileBufferWriter: BufferedWriter): String = {
-
+	def getQueryForDomain(sc: SparkContext, domain: String, dnsRecords: spark.RDD[(Int, Int, String, String, String, Int, Int, List[Array[String]], List[Array[String]], List[Array[String]])], outFile: String) = {
 		
+		val resArr = dnsRecords.filter(x => {
+			val tmp = new parseUtils().parseDomain(x._5,domain)
+			tmp == domain
+		}).toArray
+
+		if (resArr.length != 0){
+			val outFileWriter = new FileWriter(outFile, true)
+			val outFileBufferWriter = new BufferedWriter(outFileWriter)
+
+			for(record <- resArr){
+				val line = new ParseDNSFast().antiConvert(record)
+				outFileBufferWriter.write(line+"\n")
+			}
+			outFileBufferWriter.close
+		}
+	}
+
+	def getQueriesoforAllDomains(sc: SparkContext, inFile: String, outFile: String): Unit = {
+		val domainAndTypo = "./res/domainAndTypo/"
+		val domainName = convertFilename(inFile)
+
+		val tmpArr = inFile.split('/')
+		val filename = tmpArr.apply(tmpArr.length-1)
+		println("inFile: "+inFile)
+		println("domainName: "+domainName)
+		println("filename: "+filename)
+		val file = new File(domainAndTypo+domainName)
+		if(!file.exists){
+			return
+		}
+		val typoArr = io.Source.fromFile(domainAndTypo+domainName).getLines.map(x => x.split(" ")).toArray.apply(0)
+
+		val outDir = new File(outFile)
+		if(!outDir.exists){
+			outDir.mkdir
+		}
+
+		val target = typoArr.apply(0)
+		for(typo <- typoArr){
+			if(typo!=target){
+				val dnsRecords = sc.textFile(inFile, 20).map( x =>{
+					new ParseDNSFast().convert(x)
+				}).cache
+				getQueryForDomain(sc, typo, dnsRecords, outFile+"/"+typo)
+			}
+		}
+	}
+
+	def getTTL(record: (Int, Int, String, String, String, Int, Int, List[Array[String]],List[Array[String]],List[Array[String]])): Int = {
+		val domain = record._5
+		val answ_section = record._8
+		val anth_section = record._9
+		val addi_section = record._10
+
+		for(rr <- answ_section){
+			if(rr.apply(0) == domain && rr.apply(3) == "A")
+				return rr.apply(1).toInt
+		}
+
+		for(rr <- anth_section){
+			if(rr.apply(0) == domain && rr.apply(3) == "A")
+				return rr.apply(1).toInt
+		}
+
+		for(rr <- addi_section){
+			if(rr.apply(0) == domain && rr.apply(3) == "A")
+				return rr.apply(1).toInt
+		}
+
+		return 0
+	}
+
+	def getPairsFromFile(sc: SparkContext, typoArr: Array[String], inFile: File, outFileBufferWriter: BufferedWriter): String = {		
 
 		println(inFile.toString)
 		val domain = typoArr.apply(0) //the correct domain of target website
@@ -204,6 +275,20 @@ object sorted extends Serializable {
 
 		if (dnsRecords.count == 0)
 			return ""
+/*
+		val ips = dnsRecords.map(x => x._3).filter(x => x.contains(".")).map(x => {
+			val arr = x.split('.')
+			val ip = arr.apply(0)+"."+arr.apply(1)+".0.0"
+			ip
+		}).distinct
+*/
+		val ips = dnsRecords.map(x => x._3).filter(x => x.contains(".")).distinct
+		val hashMap = scala.collection.mutable.HashMap[String, Int]()
+		for(ip <- ips.toArray){
+			hashMap.+=((ip, 0))
+		}
+
+
 
 		val firstRecord = dnsRecords.first
 
@@ -216,7 +301,9 @@ object sorted extends Serializable {
 		for(domainRecord <- domainArr){
 			val time = domainRecord._1
 			val src_ip = domainRecord._3
-
+			val ttl = getTTL(domainRecord)
+			if((time+ttl)>hashMap(src_ip))
+				hashMap(src_ip) = time+ttl
 			//require all dns records in dnsRecords are sorted by timestamp
 			val windowRdd = dnsRecords.mapPartitions(itr => itr.takeWhile(
 				_._1 < time //The query for a typo domain must be present before the correct version
@@ -237,16 +324,51 @@ object sorted extends Serializable {
 						if(rdd.count!=0){
 							val tmpArr = rdd.toArray
 							for(typoRecord <- tmpArr){
-								val str1 = new ParseDNSFast().antiConvert(typoRecord)
-								val str2 = new ParseDNSFast().antiConvert(domainRecord)
-								outFileBufferWriter.write(str1+";;"+str2+"\n")
+								
+								if(typoRecord._1 > hashMap(typoRecord._3)){
+									val str1 = new ParseDNSFast().antiConvert(typoRecord)
+									val str2 = new ParseDNSFast().antiConvert(domainRecord)
+									outFileBufferWriter.write(str1+";;"+str2+"\n")
+								}
 							}
 						}
 					}
 				}
 			}
-
+/*
+			val ttl = getTTL(domainRecord)
+			//get all records in TTL
+			val windowRdd2 = dnsRecords.mapPartitions(itr => itr.takeWhile(
+				_._1 <= time+ttl
+			)).filter(r => r._1 >= time)
 			
+			val resultRdd2 = windowRdd2.filter(r => {
+				//val flag = compareIpAddr(r._3, src_ip)
+				val flag = r._3 == src_ip //Not sure whether two resolvers share their cache
+				flag
+			}).cache
+
+			if (resultRdd2.count != 0){
+				for(typo <- typoArr){
+					val rdd = resultRdd2.filter(r => {
+						val tmp = new parseUtils().parseDomain(r._5, typo)
+						tmp == typo
+					})
+					if(rdd.count!=0){
+						val tmpArr = rdd.toArray
+						for(typoRecord <- tmpArr){
+							val str1 = new ParseDNSFast().antiConvert(typoRecord)
+							val str2 = new ParseDNSFast().antiConvert(domainRecord)
+							val str3 = "CACHED BY RESOLVER"
+							val str4 = "ENDING TTL WINDOW IN 60 SEC"
+							if((time+ttl-typoRecord._1) < 60)
+								outFileBufferWriter.write(str1+";;"+str2+";;"+str3+";;"+str4+"\n")
+							else
+								outFileBufferWriter.write(str1+";;"+str2+";;"+str3+"\n")
+						}
+					}
+				}
+			}*/
 		}
 		//If there is a query for correct version at the very beginning of this file (within 60sec)
 		//it is possible that there are typo qureies for this at the previous file, the left time and 
@@ -323,7 +445,7 @@ object sorted extends Serializable {
 			if(outFile.exists){
 				outFile.delete
 			}
-*/			
+*/		
 
 			val typoArr = io.Source.fromFile(domainAndTypo+domainName).getLines.map(x => x.split(" ")).toArray.apply(0)
 
@@ -334,7 +456,7 @@ object sorted extends Serializable {
 				//var index=0
 				val logFile = new File("./log.txt")
 				if(logFile.exists){
-					logFile.delete
+				//	logFile.delete
 				}
 				
 				val writer = new FileWriter("./log.txt", true)
@@ -348,8 +470,9 @@ object sorted extends Serializable {
 				val outFileBufferWriter = new BufferedWriter(outFileWrite)
 				println("******************")
 				println("inFilePath: "+inFilePath)
-				//val line = getPairsFromFile(sc, typoArr, inFilePath, outFileBufferWriter)
-				val line  = "!23"
+				val infile1 = new File (inFilePath)
+				val line = getPairsFromFile(sc, typoArr, infile1, outFileBufferWriter)
+				
 				//Deal with the records between two files
 				val indexStr = filename.split('-')
 				val length = indexStr.apply(1).length
@@ -363,7 +486,8 @@ object sorted extends Serializable {
 					println("PreFileName: "+path+pre_filename)
 					println("outFilePath: "+outFilePath)
 					println("*****************")
-					//getLastPairsFromFile(sc, typoArr, path+pre_filename, outFileBufferWriter, line)
+					val infile2 = new File(path + pre_filename)
+					getLastPairsFromFile(sc, typoArr, infile2, outFileBufferWriter, line)
 				}
 				//previousFile.clear
 				//previousFile.append(file.toString)
@@ -419,11 +543,20 @@ object sorted extends Serializable {
 
 
 		val inFileDir2 = "./res/sortedWebFiles/"
+		println("args(0): "+args.apply(0))
 		val dirname = args.apply(0).split('/').apply(0)
 		val filename = args.apply(0).split('/').apply(1)
-		println(inFileDir2+args.apply(0))
-		println(outFileDir+args.apply(1)+"/"+dirname)
+		//println(inFileDir2+args.apply(0))
+		//println(outFileDir+args.apply(1)+"/"+dirname)
+
+		val outFileDir2 = "./res/singleTypoRecord/"
+		val outFile2 = new File(outFileDir2)
+		if(!outFile2.exists){
+			outFile2.mkdir()
+		}
+		println(outFileDir2+dirname)
 		getAllPairs(sc, inFileDir2+args.apply(0), outFileDir+args.apply(1)+"/"+dirname)
+		getQueriesoforAllDomains(sc, inFileDir2+args.apply(0), outFileDir2+dirname)
 	}
 	
 }
